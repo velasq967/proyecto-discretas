b637ea433b120edef6ce1582363e475e1f101333
s4m976 <velasq976@gmail.com>
2025-11-09 18:38:17 -0500

    commit #3

--- n-queens.py
+++ n-queens.py
@@ -6,8 +6,18 @@ Class: A
 """
 
 def main():
-    #TODO
-    pass
+    continuar = True
+    while continuar:
+        print("Selecciona lo que quieres hacer: \n" \
+        "1) Evaluar caso(s)\n"\
+        "2) Salir \n")
+        opcion = int(input("ingresa un numero:"))
+        if opcion == 2:
+            continuar = False
+            break
+        elif opcion == 1:
+            casos = int(input("cuandos casos quieres evaluar? \n"))
+            for i in range(casos):
 
 
 
@@ -19,10 +29,16 @@ def nQueens(Tablero, Queens, Lista_Coords):
     Parametros: 
 
     """
+
     return False
 
 
-def CARDINALIDAD_POSSIBLES_CONFIGURACIONES():
+def configurations():
+    """
+    funcion que evalua cuantas posibles configuraciones (subconjuntos) 
+    """
     #TODO
     pass
 
+
+main()
