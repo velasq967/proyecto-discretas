b5d0f2b604ece658241def32fc7088407270559e
s4m976 <velasq976@gmail.com>
2025-11-09 17:39:58 -0500

    commit #2

---
+++ n-queens.py
@@ -0,0 +1,28 @@
+"""
+Discrete Mathematics and Its Applications
+---------------------------------------------------
+Members: Santiago Angulo, Samuel Velasquez, Emerson Perea
+Class: A
+"""
+
+def main():
+    #TODO
+    pass
+
+
+
+def nQueens(Tablero, Queens, Lista_Coords):
+    #TODO
+    """
+    Funcion que determina si una configuracion de N reinas en un tablero N x N es valida
+
+    Parametros: 
+
+    """
+    return False
+
+
+def CARDINALIDAD_POSSIBLES_CONFIGURACIONES():
+    #TODO
+    pass
+
