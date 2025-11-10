d1d6ce4ce11d7e5d9d3ee082d25e1fe0f64de341
s4m976 <velasq976@gmail.com>
2025-11-09 19:59:57 -0500

    Main completado

--- n-queens.py
+++ n-queens.py
@@ -16,10 +16,46 @@ def main():
             continuar = False
             break
         elif opcion == 1:
-            casos = int(input("cuandos casos quieres evaluar? \n"))
+            valid = False
+            while valid == False:
+                casos = int(input("cuandos casos quieres evaluar? (1- 1000) \n"))
+                if casos <= 1000:
+                    valid = True
+                    break
+                else:
+                    print("input no valido, digite de nuevo\n")
+            
+
             for i in range(casos):
-
-
+                N = int(input("ingresa el tamaño del tablero (recuerda colocar un numero entre el 1 y el 25)\n"))
+                queens = []
+                if N >= 1 and N <= 25:
+                    for j in range(N):
+                        print("ingrese las coordenadas de la reina #", j+1 ,"\n(digite X: -1 y Y: -1 para terminar las reinas)\n")
+                        valid = False
+                        end_case = False
+                        while valid == False:
+                            CoordenadaX = int(input("ingresa la coordenada X: \n"))
+                            CoordenadaY = int(input("ingresa la coordenada Y: \n"))
+                            if (CoordenadaX <= N and CoordenadaX >= 0) and (CoordenadaY <= N and CoordenadaY >= 0):
+                                valid = True
+                                break
+                            elif CoordenadaX == -1 and CoordenadaY == -1:
+                                valid = True
+                                end_case = True
+                            else:
+                                print("este input no es valido, intente otra vez")
+                        #Coordenadas_input = int(input("Recuerda introducir dos numeros enteros separados por una coma y un espacio (ej: 1, 1)"))
+                        #Coordenadas = Coordenadas_input.split(", ")
+                        if end_case == True:
+                            break
+                        queenj_coords = [CoordenadaX, CoordenadaY]
+                        queens.append(queenj_coords)
+                        print(queens)
+                else:
+                    print("input invalido, repita porfavor")
+                       
+                        
 
 def nQueens(Tablero, Queens, Lista_Coords):
     #TODO
@@ -32,7 +68,6 @@ def nQueens(Tablero, Queens, Lista_Coords):
 
     return False
 
-
 def configurations():
     """
     funcion que evalua cuantas posibles configuraciones (subconjuntos) 
@@ -40,7 +75,4 @@ def configurations():
     #TODO
     pass
 
-
 main()
-
-#Santiago Angulo prueba commit
